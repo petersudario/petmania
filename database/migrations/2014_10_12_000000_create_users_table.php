b025d9ccd3e95b1abd6eb215039eacbfe1746bf9
Pedro Sudario <pedrohenriquesudariosilva@gmail.com>
2024-03-10 17:10:14 -0300

    Remove UNIQUE constraint from users id

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,8 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->integer('id')->autoIncrement();
             $table->string('name');
+            $table->string('role')->default('customer');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
