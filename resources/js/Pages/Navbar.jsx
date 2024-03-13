21ea5675d2cf92cea5dd7415af911289ad20f00d
Guilherme Sampaio <guisamfurquim@gmail.com>
2024-03-13 19:09:20 -0300

    melhoras na visualização dos textos

--- resources/js/Pages/Navbar.jsx
+++ resources/js/Pages/Navbar.jsx
@@ -14,7 +14,7 @@ function Navbar() {
             <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div class="relative flex h-16 items-center justify-between">
                     <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                        <button type="button" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                        <button type="button" class="relative inline-flex items-center justify-center rounded-md p-2 text-white hover:bg-gray-900 hover:text-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
                             <span class="absolute -inset-0.5"></span>
                             <span class="sr-only">Open main menu</span>
                             <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
@@ -32,9 +32,9 @@ function Navbar() {
                         <div class="hidden sm:ml-6 sm:block">
                             <div class="flex space-x-4">
                                 <a href="#" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Dashboard</a>
-                                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Team</a>
-                                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Projects</a>
-                                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Calendar</a>
+                                <a href="#" class="text-white hover:bg-gray-100 hover:text-gray-600 rounded-md px-3 py-2 text-sm font-medium">Team</a>
+                                <a href="#" class="text-white hover:bg-gray-100 hover:text-gray-600 rounded-md px-3 py-2 text-sm font-medium">Projects</a>
+                                <a href="#" class="text-white hover:bg-gray-100 hover:text-gray-600 rounded-md px-3 py-2 text-sm font-medium">Calendar</a>
                             </div>
                         </div>
                     </div>
--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -11,7 +11,7 @@ export default function Welcome({auth, laravelVersion, phpVersion}) {
         <Navbar/>
         <div class="w-100% max-w-[100%] h-100vh bg-opacity-[0.5] bg-no-repeat bg-center bg-fundo text-white md:text-lg">
             <div className="textodiv">
-                <h2>Conheça a nossa loja!</h2>
+                <h2 class="text-6xl">Conheça a nossa loja!</h2>
                 <br/>
                 <br/>
                 <br/>
@@ -29,23 +29,29 @@ export default function Welcome({auth, laravelVersion, phpVersion}) {
         </div>
         <div class="w-[100%] max-h-[100%] h-[100vh] flex bg-paleta-5 justify-around flex-row text-white md:items-center text-5xl" id="servicos">
             <img src={Logo}></img>
-            <h2 class="text-6x1">Conheça nossos serviços:</h2>
+            <div class="items-center justify-center flex flex-col">
+               <h2 class="text-6x1">Conheça nossos serviços:</h2>
+                <div class="w-[300px] h-[300px] bg-white text-paleta-2 mt-[20px] rounded-[30px] justify-center flex flex-col items-center">
+                      <p>Banho</p>
+                      <p class="text-black text-base">Um banho comprodutos de qualidade para o seu pet</p>
+                </div>
 
+            </div>
         </div>
 
         <div class="w-[100%] h-[200px] bg-gradient-to-b from-paleta-5 to-paleta-2">
         </div>
 
-        <div class="w-[100%] max-h-[100%] h-[100vh] flex bg-paleta-2 justify-center flex-row text-white md:items-center text-6xl" id="servicos">
+        <div class="w-[100%] max-h-[100%] h-[100vh] flex bg-paleta-2 justify-center flex-col text-white md:items-center text-6xl" id="servicos">
             
             <h2 class="text-6x1">Nosso diferencial:</h2>
+            <br />
+
+            <p class="text-3xl">Nosso diferencial é a facilidade de realizar o serviço para o seu pet</p>
+
+            <a>Quer saber mais, clique aqui</a>
 
         </div>
-
-        <script>
-
-        </script>
-
     </>
 );
 }
