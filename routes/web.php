5c41fb9754ba0375f2ecd915aa4b92158f72e238
andretini <andrealijaramosagostini@gmail.com>
2024-03-13 21:22:44 -0300

    Adição das rotas Agenda Pet Contact e Services

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PetController;
 use App\Http\Controllers\ContactsController;
 use App\Http\Controllers\PetOwnerController;
 use App\Http\Controllers\HomepageController;
+<<<<<<< HEAD
 
+=======
+use App\Http\Controllers\AgendaController;
+use App\Http\Controllers\ServicesController;
+>>>>>>> ed8bf7a (Adição das rotas Agenda Pet Contact e Services)
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -20,13 +26,16 @@ use Inertia\Inertia;
 |
 */
 
-
+Route::resource("agenda", AgendaController::class);
+Route::resource("pet", PetController::class);
+Route::get("/services", [ServicesController::class, 'index'])->name('services');
 Route::get("/contacts", [ContactsController::class, 'index'])->name('contacts');
 
 Route::get("/", [HomepageController::class, 'index'])->name('homepage');
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::resource('petowner', PetOwnerController::class);
 
