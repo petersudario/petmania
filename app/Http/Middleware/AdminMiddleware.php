eff07230264187503e0b54d4429e9e4521da06b4
Pedro Sudario <pedrohenriquesudariosilva@gmail.com>
2024-03-19 21:36:10 -0300

    Admin Middleware

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, ... $roles): Response
+    {
+        if (!Auth::check()) // I included this check because you have it, but it really should be part of your 'auth' middleware, most likely added as part of a route group.
+        return redirect('login');
+
+        $user = Auth::user();
+        // Check if user has the role This check will depend on how your roles are set up
+        if($user->role == "admin"){
+            return $next($request);
+        }
+
+        return redirect('services');
+    }
+}
