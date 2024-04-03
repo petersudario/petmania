53821a97c73f6161d8e133eecf70910741296a48
guiguitatu <guisamfurquim@gmail.com>
2024-04-03 16:26:22 -0300

    Mudança das telas de edit, index e Edit

--- resources/js/Pages/CRUD/Pet/Create.jsx
+++ resources/js/Pages/CRUD/Pet/Create.jsx
@@ -15,13 +15,7 @@ import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
 import SelectInput from '@/Components/SelectInput';
 import InputError from '@/Components/InputError';
-import Chorro1 from "../../../../../public/images/chorro1.png";
-import Chorro2 from "../../../../../public/images/chorro2.png";
-import Chorro3 from "../../../../../public/images/chorro3.png";
-import Chorro4 from "../../../../../public/images/chorro4.png";
-import Chorro5 from "../../../../../public/images/chorro5.png";
-import Chorro6 from "../../../../../public/images/chorro6.png";
-import Chorro7 from "../../../../../public/images/chorro7.png";
+
 
 export default function CreatePet({ auth }) {
 
